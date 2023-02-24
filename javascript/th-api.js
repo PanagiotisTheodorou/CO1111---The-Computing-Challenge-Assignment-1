beb06cd190f0c280bf9f7c5bdf174ce7e09e01e8
PanagiotisTheodorou <PTheodorou@uclan.ac.uk>
2023-02-24 10:52:12 +0200

    General update
    In this update I add javascript to show the available treasure hunts and I work more on the css

---
+++ javascript/th-api.js
@@ -0,0 +1,17 @@
+const TH_BASE_URL = "https://codecyprus.org/th/api/"; // the true API base url
+
+async function doList() {
+
+    const reply = await fetch(TH_BASE_URL + "list");
+    const json = await reply.json();
+
+    let treasureHuntsArray = json.treasureHunts;
+    let listHtml = "<ul>";
+    for(let i = 0; i < treasureHuntsArray.length; i++) {
+        listHtml +=
+            "<li>" + "<b>" + treasureHuntsArray[i].name + "</b><br/>" + "<i>" + treasureHuntsArray[i].description + "</i><br/>" + "<a href=\"javascript:select(\'" + treasureHuntsArray[i].uuid + "\')\">Start</a>" + "</li>";
+    }
+    listHtml += "</ul>";
+    // update the DOM with the newly created list
+    document.getElementById("treasureHunts").innerHTML = listHtml;
+}
